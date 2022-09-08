7201bcfaec35fead636b191792afcb5c09923ada
Alex G. Jensen <jindoohex@hotmail.com>
2022-09-08 14:38:31 +0200

    Added serveral coffee classes

--- CoffeeShopConsoleAppNet60/Program.cs
+++ CoffeeShopConsoleAppNet60/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using CoffeeShopConsoleAppNet60;
+
+CoffeeWorker theWorker = new CoffeeWorker();
+theWorker.Start();
+
+Console.WriteLine("Press any key to close the program...");
+Console.ReadKey();
